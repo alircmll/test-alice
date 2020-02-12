7e90615cf5e3d5f59750062b315ff3295315f2b8
rcmll <alice_rc@live.fr>
2020-02-12 18:23:35 +0100

    done / refacto a suivre "

--- cell.rb
+++ cell.rb
@@ -1,9 +1,11 @@
 class Cell
-  attr_reader :i, :j
+  attr_reader :tabs, :x, :y
 
-  def initialize(i, j, alive)
-    @i = i
-    @j = j
+  def initialize(tabs, x, y, alive = true)
+    @tabs = tabs
+    @new_tabs = tabs
+    @x = x
+    @y = y
     @alive = alive
   end
 
@@ -11,9 +13,57 @@ class Cell
     @alive ? 1 : 0
   end
 
-  # def n_coordinates
-  #   coords = [i - 1, i, i + 1].product([j - 1, j, j + 1])
-  #   coords.delete([i, j])
-  #   coords
-  # end
+  def n_coordinates(x, y)
+    coords = [x - 1, x, x + 1].product([y - 1, y, y + 1])
+    coords.delete([x, y])
+    coords.select { |el| el[0] >= 0 && el[0] <= 15 && el[1] >= 0 && el[1] <= 15 }
+  end
+
+  def n_count(x,y)
+    sum = 0
+    n_coordinates(x, y).each do |coord|
+      sum += 1 if tabs[coord[0]][coord[1]] == 1
+    end
+    return sum
+  end
+
+  def new_t
+    @tabs.each_with_index do |row, x|
+      row.each_with_index do |cell, y|
+        if cell == 1 && (n_count(x, y) == 2 || n_count(x, y) == 3)
+          @new_tabs[x][y] = 1
+        elsif cell == 1 && (n_count(x, y) == 1 || n_count(x, y) == 0)
+          #cell devient = 0
+          @new_tabs[x][y] = 0
+        elsif cell == 0 && n_count(x, y) == 3
+          #cell devient = 1
+          @new_tabs[x][y] = 1
+        elsif cell == 0 || cell == 1 && n_count(x, y) > 3
+          #cell devient = 0
+          @new_tabs[x][y] = 0
+        end
+      end
+    end
+    @new_tabs
+  end
 end
+
+tabs = [[0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+        [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+        [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1]]
+
+
+print Cell.new(tabs,0,0).new_t
